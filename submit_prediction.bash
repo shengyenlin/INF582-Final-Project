e51a7e8d58f00aa64eeb97c154ccbd89e61e8343
shengyenlin0501 <shengyenlin0501@gmail.com>
2024-03-12 21:49:22 +0100

    update code

---
+++ submit_prediction.bash
@@ -0,0 +1,4 @@
+kaggle competitions submit \
+    -c inf582-news-articles-title-generation \
+    -f results/submission_barthez-orangesum-title-no-finetune.csv \
+    -m "barthez-orange-sum-no-finetune"
---
+++ valid_and_test.bash
@@ -0,0 +1,8 @@
+# env_name: nlp-final
+python test_text_summarization.py \
+    --model_name moussaKam/barthez-orangesum-title \
+    --out_valid_df_name barthez-orangesum-title-no-finetune \
+    --do_valid \
+
+    # --out_test_df_name barthez-orangesum-title-no-finetune \
+    # --do_test \
